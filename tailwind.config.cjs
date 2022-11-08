d0aeda33ab2b48f3bf2a6f1ae9aefb8643d5dbba
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-11-07 20:13:31 -0800

    feat: update colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
+const { gray } = require('tailwindcss/colors');
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				gray: {
+					100: '#EFF0F0',
+					200: '#D8D9D9',
+					300: '#C0C3C3',
+					400: '#909595',
+					500: '#616868',
+					600: '#575E5E',
+					700: '#3A3E3E',
+					800: '#2C2F2F',
+					900: '#222222'
+				}
+			}
+		}
 	},
 
 	plugins: []
